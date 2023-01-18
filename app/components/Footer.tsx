8da4e5fc56e2cb708b1e7217c143ca7ffc8365f3
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-01-19 02:09:50 +0700

    final test

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,13 @@
 export const Footer = () => {
   return (
-    <footer className="font-mono text-slate-400 my-2 py-2">
-      built with 💖 by <a href="https://torn4dom4n.github.io">Long Nhat Nguyen</a>
+    <footer className="font-mono text-sm text-slate-200 my-2 py-2">
+      built with 💖 by{" "}
+      <a
+        href="https://torn4dom4n.github.io"
+        className="underline decoration-wavy hover:decoration-4"
+      >
+        Long Nhat Nguyen
+      </a>
     </footer>
   );
 };
--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,32 @@
 export default function Head() {
   return (
     <>
-      <title>Tam Dao 2023 | Retro90s</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Website of tools made for assistance" />
-      <link rel="icon" href="/favicon.ico" />
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="description" content="Tam Dao Trip 2023" />
+    <meta name="robots" content="follow, index" />
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+    <link rel="icon" href="favicon.ico" />
+
+    <title>It's Tam Dao 2023 | Retro90s</title>
+    <meta name="title" content="It's Tam Dao 2023 | Retro90s" />
+    <meta name="description" content="Tam Dao Trip 2023" />
+    <meta name="author" content="Long Nhat Nguyen" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://tamdao2023.netlify.app" />
+    <meta property="og:title" content="It's Tam Dao 2023 | Retro90s" />
+    <meta property="og:description" content="Tam Dao Trip 2023" />
+    <meta property="og:image" content="https://tamdao2023.netlify.app/banner.jpg" />
+
+    <meta name="twitter:url" content="https://tamdao2023.netlify.app" />
+    <meta name="twitter:title" content="It's Tam Dao 2023 | Retro90s" />
+    <meta name="twitter:description" content="Tam Dao Trip 2023" />
+    <meta name="twitter:image" content="https://tamdao2023.netlify.app/banner.jpg" />
+    <meta name="twitter:site" content="@torn4dom4n" />
+    <meta name="twitter:card" content="summary_large_image" />
     </>
   )
 }
