1d1cac2e0dc7aceb863c32cbd3665780fe85801d
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-01-19 01:47:16 +0700

    add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="font-mono text-slate-400 my-2 py-2">
+      built with 💖 by <a href="https://torn4dom4n.github.io">Long Nhat Nguyen</a>
+    </footer>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { promises as fs } from "fs";
 import path from "path";
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 import { Gallery } from "./components/Gallery";
 
 const Home = async () => {
@@ -11,6 +12,7 @@ const Home = async () => {
     <div className="flex flex-col container items-center justify-center mx-auto px-2 py-4">
       <Header Title={"Tam Đảo 2023"} />
       <Gallery images={imageFilenames} />
+      <Footer />
     </div>
   );
 };
