a3a40fe02b2c39b23a1fa515b56af34c51d41f87
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-01-18 23:33:19 +0700

    remove tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin");
-
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/aspect-ratio"),
-    require("daisyui"),
-  ],
+  plugins: [],
 };
