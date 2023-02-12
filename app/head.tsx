34e651ecd80bb20a297c9dec7e31d84ecd84c370
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-02-13 03:37:46 +0700

    fix typo

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,6 @@ export default function Head() {
   return (
     <>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width,initial-scale=1" />
       <meta name="description" content="Kỷ niệm chuyến đi Tam Đảo 2023" />
       <meta name="robots" content="follow, index" />
       <link
