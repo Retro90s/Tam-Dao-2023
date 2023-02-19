25fb138a38041d3a55d849227ed73eb42b1b0c17
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-02-20 01:23:09 +0700

    remove blur images

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -1,20 +1,6 @@
 "use client";
 import Image from "next/image";
 
-const keyStr =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-
-const triplet = (e1: number, e2: number, e3: number) =>
-  keyStr.charAt(e1 >> 2) +
-  keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
-  keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
-  keyStr.charAt(e3 & 63);
-
-const rgbDataURL = (r: number, g: number, b: number) =>
-  `data:image/gif;base64,R0lGODlhAQABAPAA${
-    triplet(0, r, g) + triplet(b, 255, 255)
-  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
-
 export const Gallery = ({ images }: { images: Array<string> }) => {
   return (
     <>
@@ -24,8 +10,6 @@ export const Gallery = ({ images }: { images: Array<string> }) => {
           className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110 mb-5"
           style={{ transform: "translate3d(0, 0, 0)" }}
           src={`/pictures/${id}`}
-          placeholder="blur"
-          blurDataURL={rgbDataURL(19, 21, 19)}
           width={720}
           height={480}
           sizes="(max-width: 640px) 100vw,
