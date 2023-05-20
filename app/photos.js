0e9e61aeeabaced4d1202e58e85d0bcc66890878
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-05-20 17:09:21 +0700

    support plaiceholder

---
+++ app/photos.js
@@ -0,0 +1,559 @@
+const photos = [
+  {
+    src: '230114_165001.jpg',
+    width: 2202,
+    height: 1652,
+  },
+  {
+    src: '230114_165002.jpg',
+    width: 2114,
+    height: 1586,
+  },
+  {
+    src: '230114_165301.jpg',
+    width: 1274,
+    height: 956,
+  },
+  {
+    src: '230114_165302.jpg',
+    width: 1274,
+    height: 956,
+  },
+  {
+    src: '230114_165305.jpg',
+    width: 1205,
+    height: 904,
+  },
+  {
+    src: '230114_165306.jpg',
+    width: 1173,
+    height: 880,
+  },
+  {
+    src: '230114_165310.jpg',
+    width: 2042,
+    height: 1530,
+  },
+  {
+    src: '230114_165312.jpg',
+    width: 2506,
+    height: 1880,
+  },
+  {
+    src: '230114_165316.jpg',
+    width: 2250,
+    height: 1688,
+  },
+  {
+    src: '230114_165701.jpg',
+    width: 839,
+    height: 839,
+  },
+  {
+    src: '230114_165703.jpg',
+    width: 960,
+    height: 960,
+  },
+  {
+    src: '230114_170501.jpg',
+    width: 889,
+    height: 1185,
+  },
+  {
+    src: '230114_170504.jpg',
+    width: 749,
+    height: 999,
+  },
+  {
+    src: '230114_170506.jpg',
+    width: 809,
+    height: 1078,
+  },
+  {
+    src: '230114_170510.jpg',
+    width: 771,
+    height: 771,
+  },
+  {
+    src: '230114_170901.jpg',
+    width: 937,
+    height: 703,
+  },
+  {
+    src: '230114_170902.jpg',
+    width: 794,
+    height: 793,
+  },
+  {
+    src: '230114_170909.jpg',
+    width: 899,
+    height: 899,
+  },
+  {
+    src: '230114_170910.jpg',
+    width: 960,
+    height: 960,
+  },
+  {
+    src: '230114_171001.jpg',
+    width: 1119,
+    height: 897,
+  },
+  {
+    src: '230114_171003.jpg',
+    width: 1221,
+    height: 915,
+  },
+  {
+    src: '230114_171005.jpg',
+    width: 867,
+    height: 1157,
+  },
+  {
+    src: '230114_171007.jpg',
+    width: 920,
+    height: 1227,
+  },
+  {
+    src: '230114_171009.jpg',
+    width: 896,
+    height: 1194,
+  },
+  {
+    src: '230114_171012.jpg',
+    width: 960,
+    height: 960,
+  },
+  {
+    src: '230114_171014.jpg',
+    width: 908,
+    height: 908,
+  },
+  {
+    src: '230114_171018.jpg',
+    width: 1276,
+    height: 956,
+  },
+  {
+    src: '230114_171019.jpg',
+    width: 1075,
+    height: 806,
+  },
+  {
+    src: '230114_171020.jpg',
+    width: 928,
+    height: 696,
+  },
+  {
+    src: '230114_171022.jpg',
+    width: 938,
+    height: 1251,
+  },
+  {
+    src: '230114_171520.jpg',
+    width: 1388,
+    height: 1850,
+  },
+  {
+    src: '230114_171522.jpg',
+    width: 1390,
+    height: 1390,
+  },
+  {
+    src: '230114_171628.jpg',
+    width: 1863,
+    height: 1398,
+  },
+  {
+    src: '230114_173028.jpg',
+    width: 1836,
+    height: 1836,
+  },
+  {
+    src: '230114_173029.jpg',
+    width: 1747,
+    height: 2328,
+  },
+  {
+    src: '230114_173030.jpg',
+    width: 1786,
+    height: 2381,
+  },
+  {
+    src: '230114_173031.jpg',
+    width: 1861,
+    height: 1395,
+  },
+  {
+    src: '230114_173032.jpg',
+    width: 1996,
+    height: 1540,
+  },
+  {
+    src: '230114_173035.jpg',
+    width: 2276,
+    height: 1804,
+  },
+  {
+    src: '230114_173601.jpg',
+    width: 2568,
+    height: 1926,
+  },
+  {
+    src: '230114_173606.jpg',
+    width: 2568,
+    height: 1926,
+  },
+  {
+    src: '230114_173608.jpg',
+    width: 2568,
+    height: 1926,
+  },
+  {
+    src: '230114_173610.jpg',
+    width: 2182,
+    height: 1637,
+  },
+  {
+    src: '230114_173612.jpg',
+    width: 2068,
+    height: 1551,
+  },
+  {
+    src: '230114_173617.jpg',
+    width: 1596,
+    height: 1197,
+  },
+  {
+    src: '230114_173620.jpg',
+    width: 1373,
+    height: 1030,
+  },
+  {
+    src: '230114_173622.jpg',
+    width: 1610,
+    height: 1327,
+  },
+  {
+    src: '230114_174015.jpg',
+    width: 1712,
+    height: 2284,
+  },
+  {
+    src: '230114_174016.jpg',
+    width: 1711,
+    height: 2281,
+  },
+  {
+    src: '230114_174017.jpg',
+    width: 1702,
+    height: 1702,
+  },
+  {
+    src: '230114_174020.jpg',
+    width: 1852,
+    height: 2470,
+  },
+  {
+    src: '230114_174021.jpg',
+    width: 1728,
+    height: 2305,
+  },
+  {
+    src: '230114_174022.jpg',
+    width: 1723,
+    height: 2297,
+  },
+  {
+    src: '230114_174040.jpg',
+    width: 1764,
+    height: 2352,
+  },
+  {
+    src: '230114_174041.jpg',
+    width: 1785,
+    height: 2380,
+  },
+  {
+    src: '230114_174545.jpg',
+    width: 1808,
+    height: 2410,
+  },
+  {
+    src: '230114_174546.jpg',
+    width: 1822,
+    height: 2430,
+  },
+  {
+    src: '230114_174547.jpg',
+    width: 1701,
+    height: 2268,
+  },
+  {
+    src: '230114_174622.jpg',
+    width: 1859,
+    height: 2480,
+  },
+  {
+    src: '230114_174623.jpg',
+    width: 1920,
+    height: 2560,
+  },
+  {
+    src: '230114_174624.jpg',
+    width: 1825,
+    height: 2433,
+  },
+  {
+    src: '230114_180001.jpg',
+    width: 1197,
+    height: 898,
+  },
+  {
+    src: '230114_180002.jpg',
+    width: 1193,
+    height: 896,
+  },
+  {
+    src: '230114_180007.jpg',
+    width: 1251,
+    height: 939,
+  },
+  {
+    src: '230114_180009.jpg',
+    width: 1199,
+    height: 900,
+  },
+  {
+    src: '230114_183001.jpg',
+    width: 851,
+    height: 851,
+  },
+  {
+    src: '230114_191301.jpg',
+    width: 1024,
+    height: 576,
+  },
+  {
+    src: '230114_191302.jpg',
+    width: 1024,
+    height: 576,
+  },
+  {
+    src: '230114_191303.jpg',
+    width: 1024,
+    height: 576,
+  },
+  {
+    src: '230114_191305.jpg',
+    width: 1024,
+    height: 576,
+  },
+  {
+    src: '230114_191306.jpg',
+    width: 1024,
+    height: 576,
+  },
+  {
+    src: '230114_191307.jpg',
+    width: 1024,
+    height: 576,
+  },
+  {
+    src: '230114_191308.jpg',
+    width: 1024,
+    height: 576,
+  },
+  {
+    src: '230114_191401.jpg',
+    width: 884,
+    height: 1178,
+  },
+  {
+    src: '230114_191403.jpg',
+    width: 884,
+    height: 1179,
+  },
+  {
+    src: '230114_191501.jpg',
+    width: 847,
+    height: 1130,
+  },
+  {
+    src: '230114_191502.jpg',
+    width: 886,
+    height: 1181,
+  },
+  {
+    src: '230114_191703.jpg',
+    width: 960,
+    height: 960,
+  },
+  {
+    src: '230114_191704.jpg',
+    width: 960,
+    height: 960,
+  },
+  {
+    src: '230114_191705.jpg',
+    width: 960,
+    height: 960,
+  },
+  {
+    src: '230114_191932.jpg',
+    width: 1021,
+    height: 765,
+  },
+  {
+    src: '230114_191933.jpg',
+    width: 1021,
+    height: 765,
+  },
+  {
+    src: '230114_191935.jpg',
+    width: 1215,
+    height: 912,
+  },
+  {
+    src: '230114_191939.jpg',
+    width: 1021,
+    height: 765,
+  },
+  {
+    src: '230114_191940.jpg',
+    width: 1128,
+    height: 846,
+  },
+  {
+    src: '230114_191942.jpg',
+    width: 1132,
+    height: 849,
+  },
+  {
+    src: '230114_191943.jpg',
+    width: 1021,
+    height: 765,
+  },
+  {
+    src: '230114_192001.jpg',
+    width: 1280,
+    height: 960,
+  },
+  {
+    src: '230114_192002.jpg',
+    width: 1280,
+    height: 960,
+  },
+  {
+    src: '230114_192003.jpg',
+    width: 1280,
+    height: 960,
+  },
+  {
+    src: '230114_212739.jpg',
+    width: 1728,
+    height: 972,
+  },
+  {
+    src: '230114_232732.jpg',
+    width: 4032,
+    height: 3024,
+  },
+  {
+    src: '230114_232738.jpg',
+    width: 4032,
+    height: 3024,
+  },
+  {
+    src: '230114_232740.jpg',
+    width: 4032,
+    height: 3024,
+  },
+  {
+    src: '230114_234658.jpg',
+    width: 4032,
+    height: 3024,
+  },
+  {
+    src: '230115_081512.jpg',
+    width: 2468,
+    height: 1851,
+  },
+  {
+    src: '230115_082030.jpg',
+    width: 2560,
+    height: 1920,
+  },
+  {
+    src: '230115_082031.jpg',
+    width: 2560,
+    height: 1920,
+  },
+  {
+    src: '230115_082032.jpg',
+    width: 2407,
+    height: 1805,
+  },
+  {
+    src: '230115_090730.jpg',
+    width: 4032,
+    height: 3024,
+  },
+  {
+    src: '230115_110901.jpg',
+    width: 957,
+    height: 956,
+  },
+  {
+    src: '230115_110902.jpg',
+    width: 957,
+    height: 956,
+  },
+  {
+    src: '230115_111002.jpg',
+    width: 1176,
+    height: 883,
+  },
+  {
+    src: '230115_111004.jpg',
+    width: 1189,
+    height: 892,
+  },
+  {
+    src: '230115_111006.jpg',
+    width: 842,
+    height: 1123,
+  },
+  {
+    src: '230115_111008.jpg',
+    width: 1127,
+    height: 845,
+  },
+  {
+    src: '230115_111201.jpg',
+    width: 1243,
+    height: 932,
+  },
+  {
+    src: '230115_111202.jpg',
+    width: 1248,
+    height: 936,
+  },
+  {
+    src: '230115_111206.jpg',
+    width: 1239,
+    height: 929,
+  },
+  {
+    src: '230115_111209.jpg',
+    width: 1236,
+    height: 927,
+  },
+  {
+    src: '230115_111210.jpg',
+    width: 1231,
+    height: 923,
+  },
+]
+
+export default photos
