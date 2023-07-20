c03871680b5ac70aa80adff27068b658431ae1cc
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-07-20 16:25:40 +0700

    Final version

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Logo from '@/components/Logo';
+import Icon from '@/components/Icon';
 import Gallery from '@/components/Gallery';
 
 const title = 'KỶ NIỆM TAM ĐẢO 2023';
-const description = 'Tam Đảo 2023, cùng nhau chúng tôi tạo nên những kỷ niệm.';
+const description = 'Tam Đảo, ngày 14 tháng 01 năm 2023';
 const author = 'Long Nhat Nguyen';
 const authorURL = 'https://torn4dom4n.github.io';
 
@@ -14,7 +14,7 @@ export default function Home() {
           <div className='after:content shadow-highlight after:shadow-highlight relative col-span-1 row-span-3 flex flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white after:pointer-events-none after:absolute after:inset-0 after:rounded-lg sm:col-span-2 lg:col-span-1 lg:row-span-2 lg:pt-0'>
             <div className='absolute inset-0 flex items-center justify-center opacity-20'>
               <span className='flex max-h-full max-w-full items-center justify-center'>
-                <Logo />
+                <Icon />
               </span>
               <span className='absolute inset-x-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black'></span>
             </div>
